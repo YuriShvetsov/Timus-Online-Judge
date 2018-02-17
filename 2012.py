9470268272f290ec0582335249b51fa7ae8f8004
Yuri <shvetsov.yuri.new@gmail.com>
2018-02-17 17:58:41 +0500

    Create 2012.py

---
+++ 2012.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''2012. About Grisha N.'''
+
+time = 240
+problems = 12
+grisha = int(input())
+
+if ((problems - grisha) * 45) <= time:
+    print('YES')
+else:
+    print('NO')
