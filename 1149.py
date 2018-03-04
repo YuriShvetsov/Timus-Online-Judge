f5f6c2233168c1ec8d418709aef456b86f6733b3
Yuri <shvetsov.yuri.new@gmail.com>
2018-03-04 15:25:53 +0500

    Create 1149.py

---
+++ 1149.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''1149. Sinus Dances'''
+
+n = int(input())
+
+def An(num):
+    sin = 'sin('
+    k = 1
+    while k < num:
+        if k % 2 != 0:
+            sin += str(k) + '-sin('
+        else:
+            sin += str(k) + '+sin('
+        k += 1
+
+    if num % 2 != 0:
+        sin += str(num) + ')'*num
+    else:
+        sin += str(num) + ')'*num
+
+    return sin
+
+def Sn(num):
+    result = '('*(num - 1)
+    k = 1
+    while k < num:
+        result += str(An(k)) + '+' + str(num + 1 - k) + ')'
+        k += 1
+
+    result += str(An(num)) + '+1'
+
+    return result
+
+print(Sn(n))
