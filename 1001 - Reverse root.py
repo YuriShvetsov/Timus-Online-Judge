158e14ad4471770d01e1d116ee9bc3ef898a042e
Yuri <shvetsov.yuri.new@gmail.com>
2018-03-06 20:58:48 +0500

    Update and rename 1001.py to 1001 - Reverse root.py

--- 1001 - Reverse root.py
+++ 1001 - Reverse root.py
@@ -2,16 +2,31 @@
 # -*- coding: utf-8 -*-
 '''1001. Reverse root'''
 
-import sys, math
+import math
+import sys
+
+
 a = []
 for line in sys.stdin:
     for i in line.split():
         a.append(int(i))
+        
 a = reversed(a)
+
 for i in a:
     print('%.4f' % math.sqrt(i))
 
+    
+# This other solution:
 
+# import sys
+
+
+# a = []
+# for line in sys.stdin:
+    # for i in line.split():
+        # a.append(int(i))
+        
 # a = reversed(a)
 # x = 1/2
 
